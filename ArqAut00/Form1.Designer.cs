f826fb6408b0614d1b22dfde42adfdfaa7feb72d
Victor Santos <vhos08@gmail.com>
2020-01-27 11:33:40 -0300

    Trabalhando com OpenFileDialog

---
+++ ArqAut00/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ArqAut00
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_CarregaArq = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bt_CarregaArq
+            // 
+            this.bt_CarregaArq.Location = new System.Drawing.Point(94, 33);
+            this.bt_CarregaArq.Name = "bt_CarregaArq";
+            this.bt_CarregaArq.Size = new System.Drawing.Size(130, 41);
+            this.bt_CarregaArq.TabIndex = 0;
+            this.bt_CarregaArq.Text = "Open File";
+            this.bt_CarregaArq.UseVisualStyleBackColor = true;
+            this.bt_CarregaArq.Click += new System.EventHandler(this.bt_CarregaArq_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 101);
+            this.Controls.Add(this.bt_CarregaArq);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_CarregaArq;
+    }
+}
+
---
+++ ArqAut00/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace ArqAut00
+{
+    public partial class Form1 : Form
+    {        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_CarregaArq_Click(object sender, EventArgs e)
+        {
+            string caminhoArquivoEntrada;
+
+            try
+            {                
+                //CRIA OBJETO
+                OpenFileDialog ofd = new OpenFileDialog();
+
+                //DEFINE AS PROPRIEDADES DE CONTROLE           
+                ofd.Multiselect = false;
+                ofd.Title = "Selecionar Arquivo";
+                ofd.InitialDirectory = @"C:/Users/Public/Documents";
+                ofd.Filter = "All files (*.*)|*.*";
+                ofd.CheckFileExists = true;
+                ofd.CheckPathExists = true;
+                ofd.RestoreDirectory = true;
+                ofd.ReadOnlyChecked = true;
+                ofd.ShowReadOnly = true;
+
+                //ABRE CAIXA DE SELEÇAO
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    caminhoArquivoEntrada = ofd.FileName;
+                    //nomeBASELINE = System.IO.Path.GetFileName(caminhoArquivoEntrada);
+                }
+                else
+                    caminhoArquivoEntrada = "";
+
+                //tb_caminhoBS.Text = caminhoArquivoEntrada;
+
+                //VERIFICA SE CAMINHO FOI PREENCHIDO CORRETAMENTE
+                if (!String.IsNullOrEmpty(caminhoArquivoEntrada))
+                    if (File.Exists(caminhoArquivoEntrada))
+                        montaAqrSaida(caminhoArquivoEntrada);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void montaAqrSaida(string caminhoArquivo)
+        {
+
+        }        
+
+    }
+}
